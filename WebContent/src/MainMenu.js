6ce67b08673ff52730cda6d758fd2e3d166485dd
Mickaël Foucaux <mickael.foucaux@gmail.com>
2016-01-30 11:52:06 +0100

    [music] disable main menu music from loading and main menu

--- WebContent/src/MainMenu.js
+++ WebContent/src/MainMenu.js
@@ -14,8 +14,8 @@ BasicGame.MainMenu.prototype = {
 		//	Here all we're doing is playing some music and adding a picture and button
 		//	Naturally I expect you to do something significantly better :)
 
-		this.music = this.add.audio('titleMusic');
-		this.music.play( '', 0, 1, true, true );
+		// this.music = this.add.audio('titleMusic');
+		// this.music.play( '', 0, 1, true, true );
 
 		this.add.sprite(0, 0, 'titlepage');
 
--- WebContent/src/Preloader.js
+++ WebContent/src/Preloader.js
@@ -27,7 +27,7 @@ BasicGame.Preloader.prototype = {
 		this.load.image('titlepage', 'images/ui/title.jpg');
 		// this.load.image('playButton', 'images/play_button.png');
 		this.load.atlas('playButton', 'images/ui/play_button.png', 'images/ui/play_button.json');
-		this.load.audio('titleMusic', ['audio/main_menu.mp3']);
+		// this.load.audio('titleMusic', ['audio/main_menu.mp3']);
 		// this.load.bitmapFont('caslon', 'fonts/caslon.png', 'fonts/caslon.xml');
 		//	+ lots of other required assets here
 		this.load.image('star', 'images/game/star.png');
